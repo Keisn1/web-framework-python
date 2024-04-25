20dda51e85e2b868e1e66c993c344b5bdfa231f5
Kay Freyer <kay.freyer@icloud.com>
2024-04-25 18:38:03 +0200

    Add docs

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/Keisn1/web-framework-python.git"
 EMAIL = "kay.freyer@icloud.com"
 AUTHOR = "Kay Freyer"
 REQUIRES_PYTHON = ">=3.11.0"
-VERSION = "0.1.0"
+VERSION = "0.0.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
